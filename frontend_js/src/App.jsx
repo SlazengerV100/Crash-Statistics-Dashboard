2416296c215c09d34da3eb34c323da05d7f42690
Z'Arn <119647505+ZianPayne@users.noreply.github.com>
2025-04-27 12:09:21 +1200

    Fixing Styles Factors, Removing Predictor Page

--- frontend_js/src/App.jsx
+++ frontend_js/src/App.jsx
@@ -8,7 +8,6 @@ import {
 import HomePage from './pages/HomePage';
 import TimeSeriesPage from './pages/TimeSeriesPage';
 import FactorsPage from './pages/FactorsPage';
-import PredictorPage from './pages/PredictorPage';
 import HelpPage from './pages/HelpPage';
 import NavBar from './components/navigation/NavBar';
 import Footer from './components/navigation/Footer';
@@ -23,7 +22,6 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/time-series" element={<TimeSeriesPage />} />
           <Route path="/factors" element={<FactorsPage />} />
-          <Route path="/predictor" element={<PredictorPage />} />
           <Route path="/help" element={<HelpPage />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
